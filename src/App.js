b93afd305a663ce00c56cdb6ba113474514679f5
David Sotto Mayor <ddsmmf@gmail.com>
2020-11-29 08:56:33 -0400

    Working with dynamic content into JSX

--- src/App.js
+++ src/App.js
@@ -5,14 +5,10 @@ import Person from './Person/Person';
 class App extends Component {
   render() {
     return (
-      // In this form the code is much maintainable and reusable and configurable!! Amazing...
       <div className="App">
         <h1>Hello World!</h1>
         <p>This is realy working!</p>
-        <Person/>
-        <Person/>
-        <Person/>
-        <Person/>        
+        <Person/>       
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const person = () => {
 
+    // Dynamic content in JSX, we use '{expression}' into JSX.
     return(
-        <p>I'm a person! My name is David and my age is 20 years.</p>
+        <p>I'm a person! My name is David and my age is {Math.floor(Math.random() * 100)} years.</p>
     );
 
 };
