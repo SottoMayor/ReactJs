b81317d59a9cd2686d205dce78339bcba5c20e1e
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-22 10:55:29 -0400

    migrating the project to react class-based and understand lifecycle hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,31 +1,30 @@
 import React from 'react';
+
 import classes from './Cockpit.css';
 
-const cockpit = props => {
-
+const cockpit = ( props ) => {
     const assignedClasses = [];
-
     let btnClass = '';
-
-    if(props.showPersons){
-        btnClass = classes.Red
+    if (props.showPersons) {
+        btnClass = classes.Red;
     }
 
-    if(props.persons.length <=2 ){
-      assignedClasses.push(classes.red);
+    if ( props.persons.length <= 2 ) {
+      assignedClasses.push( classes.red ); 
     }
-    if(props.persons.length <= 1){
-      assignedClasses.push(classes.bold)
+    if ( props.persons.length <= 1 ) {
+      assignedClasses.push( classes.bold ); 
     }
 
     return (
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is realy working!</p>
-            <button className={btnClass} onClick={() => props.toggled()}>Persons toggle</button>
+            <p className={assignedClasses.join( ' ' )}>This is really working!</p>
+            <button
+                className={btnClass}
+                onClick={props.clicked}>Toggle Persons</button>
         </div>
     );
-
-}
+};
 
 export default cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,83 +1,93 @@
-import React, { useState } from 'react';
-import  classes from './App.css';
+import React, { Component } from 'react';
+
+import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-const app = props => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
 
-  const [personsState, setPersonsState] = 
-      useState ({
-      persons: [
-        {name: 'David', age: 20, id:'person1'},
-        {name: 'Walter', age: 23, id:'person2'}
-      ]
-    })
+  state = {
+    persons: [
+      { id: 'Person1', name: 'David', age: 20 },
+      { id: 'Person2', name: 'Walter', age: 23 },
+      { id: 'Person3', name: 'Victor', age: 22 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  };
 
-  const [showPersons, setShowPersons] = useState( {showPersons: false} );
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
 
-  const togglePersons = () => {
-    const doesChange = showPersons.showPersons
-    setShowPersons({
-      showPersons: !(doesChange)
-    })
-    }
+  componentWillMount() {
+    console.log('[App.js] componentWillMount');
+  }
 
-  const changeNameHandler = (event, id) => {
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
 
-    const personIndex = personsState.persons.findIndex( person => {
-      return person.id === id;
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
 
     const person = {
-      ...personsState.persons[personIndex]
+      ...this.state.persons[personIndex]
     };
 
     person.name = event.target.value;
 
-    const persons = [...personsState.persons]; 
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
 
-    persons[personIndex] = person; 
+    this.setState({ persons: persons });
+  };
 
-    setPersonsState({
-      persons: persons
-    })
-  }
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
 
-  const deletePerson = (index) => {
-    // Good practice: create a array copy and work with it...
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
-    const persons = [...personsState.persons];
-    persons.splice(index, 1)
-    setPersonsState({
-      persons: persons
-    })
-  }
-
-
+  render() {
+    console.log('[App.js] render');
     let persons = null;
 
-    if (showPersons.showPersons){
-      persons = 
-      (
-        <div>
-
-          <Persons persons={personsState.persons} clicked={deletePerson} changed={changeNameHandler} />
-
-        </div>
-      )
-
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
     }
 
-
-  return (
-    <div className={classes.App}>
-      
-      <Cockpit title={props.appTitle} showPersons={showPersons.showPersons} persons={personsState.persons} toggled={togglePersons} />
-
-      {persons}
-        
-    </div>
-    )
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          title={this.props.appTitle}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}
+        />
+        {persons}
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
