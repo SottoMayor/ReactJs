0817522c6463f36177ea1fc29dcab037554a64bf
David Sotto Mayor <ddsmmf@gmail.com>
2020-12-22 13:37:51 -0400

    controlling the useEffect() behavior

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -6,7 +6,15 @@ const cockpit = ( props ) => {
 
     useEffect( () => {
       console.log('[Cockpit.Js] useEffect');
-    } )
+      //HTTP request .....
+
+      setTimeout( () => {
+        alert('Saved data to cloud! :)');
+      }, 1000)
+      // The array [a, b, c] in the second argument controls the dependencies...
+      // Ex: [props.person] controls the dependencies of persons!
+      // in that way, if we set [] we exect this component only in the first render time!!!!
+    }, [])
 
     const assignedClasses = [];
     let btnClass = '';
