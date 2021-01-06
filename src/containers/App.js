88d43df3ea33b65c53531bb497eafc51d810205d
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-06 13:43:20 -0400

    understanding prop chain problems

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,8 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     setCockpit: true,
-    changeCounter: 0
+    changeCounter: 0,
+    authenticated: false
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -79,6 +80,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({authenticated: true});
+  }
+
   render() {
     console.log('[App.js] render');
     let persons = null;
@@ -89,6 +94,7 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
@@ -105,6 +111,7 @@ class App extends Component {
           showPersons={this.state.showPersons}
           personsLenght={this.state.persons.lenght}
           clicked={this.togglePersonsHandler}
+          login={this.loginHandler}
           />
           : null
         }
