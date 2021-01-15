9a03ef3d1c24d4910ed23360c0998c83cd0fbc1f
David Sotto Mayor <ddsmmf@gmail.com>
2021-01-15 11:02:22 -0400

    calculating the ingredient sum dynamically

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -1,3 +1,4 @@
+import { element } from 'prop-types';
 import React from 'react';
 
 import classes from './Burguer.module.css';
@@ -6,12 +7,19 @@ import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
 
 const burguer = props => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     .map(ingKey => {
        return [...Array(props.ingredients[ingKey])].map((_, index) => {
            return <BurguerIngredient key={ingKey + index} type={ingKey} />
         })
     })
+    .reduce((arr, element) => {
+        return arr.concat(element)
+    }, []);
+
+    if( transformedIngredients.length === 0 ){
+        transformedIngredients = <p>Please add some ingredients!</p>
+    }
 
     return(
         <div className={classes.Burguer}>
